87f05031ea8a8753474c3bb186c5d630565d54d0
anda <andakadriu20@gmail.com>
2021-10-06 00:49:53 +0200

    Initial commit

---
+++ src/ict/kosovo/growth/VijushmeriaNeProvim.java
@@ -0,0 +1,27 @@
+package ict.kosovo.growth;
+import java.util.Scanner;
+public class VijushmeriaNeProvim {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Sheno moshen:");
+        int mosha = scan.nextInt();
+
+        System.out.println("Sheno Gjinin 'Femer-F ndesa Mashkull-M");
+        String gjinia = scan.next();
+
+        System.out.println("Sheno statusin martesor (P ose J) ");
+        String statusiMartesor = scan.next();
+
+        if(gjinia.equals("F")){
+            System.out.println("Do te punoni vetem ne zonat Urbane");
+        }else if(gjinia.equals("M") && mosha<40 && mosha>21){
+            System.out.println("Ju mund te punoni kudo");
+        }else if(gjinia.equals("M") && mosha>40 && mosha<61){
+            System.out.println("Ju mund te punoni vetem ne zonat rurale");
+        }else{
+            System.out.println("GABIM");
+        }
+
+    }
+}
